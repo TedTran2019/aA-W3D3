6f7e19c35a8f0bbe1c929fdc8552eef88ec18c22
Ted Tran <TedTran2019@gmail.com>
2019-09-25 17:46:30 -0700

    Finished associations exercise

---
+++ associations/app/models/enrollment.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Enrollment < ApplicationRecord
+  belongs_to :user,
+  foreign_key: :student_id
+
+  belongs_to :course
+end
